11d48f0b30a89a6ae45289e3a1aab61c68f0d3b9
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2020-05-05 12:17:14 +0200

    hujh

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    bool hasExploded = false;
+    public float delay = 3f;
+    float countdown;
+    public GameObject explosionEffect;
+
+
+    void Start()
+    {
+    
+        countdown = delay; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
