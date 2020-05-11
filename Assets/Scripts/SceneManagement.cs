46b69d4f40a1dfb9952666ad321951e4385fefbd
Razeonic <39726375+Razeonic@users.noreply.github.com>
2020-05-11 13:06:07 +0200

    added scenemanager and new scene

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+    public bool loadNextScene = false;
+    void Update()
+    {
+        if (loadNextScene)
+        {
+            Debug.Log("load: SceneTwo");
+            SceneManager.LoadScene(1);
+        }
+
+
+    }
+}
