89ff1815e65608a106ebb630487274a61a891eb9
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2020-05-13 13:24:40 +0200

    scoby

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -5,9 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
+
+    AudioSource m_explosion;
+    public bool playsound = false;
+    public float delaytimer = 3f;
+    public int levelid = 3;
     void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(3);
-    }
+        if (playsound)
+        {
+            m_explosion = GetComponent<AudioSource>();
+            m_explosion.Play();
+        }
+        
 
+        StartCoroutine(Delay());
+        
+    }
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(delaytimer);
+        SceneManager.LoadScene(levelid);
+    }
 }
